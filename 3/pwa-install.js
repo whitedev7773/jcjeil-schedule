1b35ee3601c50db5ee4cc85ece772573d80e7244
WhiteDev7773 <jkw04257773@gmail.com>
2023-03-01 01:24:23 +0900

    Service Worker 수정

--- 3/pwa-install.js
+++ 3/pwa-install.js
@@ -1,6 +1,6 @@
 if('serviceWorker' in navigator){
   try {
-    navigator.serviceWorker.register('./sw.js');
+    navigator.serviceWorker.register('../sw.js');
     console.log("Service Worker Registered");
   } catch (error) {
     console.log("Service Worker Registration Failed");
